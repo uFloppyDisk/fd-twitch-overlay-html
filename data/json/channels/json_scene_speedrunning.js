5fb93f9f1b5571d6059b033e76eb13bd5929ec22
uFloppyDisk <floppydiskproductions@outlook.com>
2020-04-28 20:51:58 -0700

    Added speedrunning json;

---
+++ data/json/channels/json_scene_speedrunning.js
@@ -0,0 +1,19 @@
+var JSON_SCENE_SPEEDRUNNING = {
+    "scene": "scene_speedrunning",
+
+    "name": "",
+    "number": 1337,
+    "logo": "res/1x/scene_vr.png",
+
+    "episode" : {
+        "titles": [
+            "VR Rangers",
+            "Death Standing",
+            "Inside The Game: Inside There [In There]",
+            "Inception",
+            "Inception: The Receptionist's Desk",
+            "Inception: Contraception"
+        ],
+        "durations": [30, 60, 90, 120, 180]
+    }
+}
